9e8f10d2e606810538b4f643235c20a9cb3d2372
王成龙 <15963685463@163.com>
2021-09-22 17:59:21 +0800

    synchronized关键字的试用

--- src/main/java/thread/UnSafeBank.java
+++ src/main/java/thread/UnSafeBank.java
@@ -8,73 +8,77 @@ import lombok.Data;
  * @author wangcl
  */
 public class UnSafeBank {
-	public static void main(String[] args) {
-		Account account = new Account(100, "买房基金");
-		WithdrawMoney withdrawMoney = new WithdrawMoney(account, 50, "我");
-		WithdrawMoney withdrawMoney1 = new WithdrawMoney(account, 80, "古力娜扎");
-		new Thread(withdrawMoney).start();
-		new Thread(withdrawMoney1).start();
+    public static void main(String[] args) {
+        Account account = new Account(100, "买房基金");
+        WithdrawMoney withdrawMoney = new WithdrawMoney(account, 50, "我");
+        WithdrawMoney withdrawMoney1 = new WithdrawMoney(account, 80, "古力娜扎");
+        new Thread(withdrawMoney).start();
+        new Thread(withdrawMoney1).start();
 
-	}
+    }
 }
 
 @Data
 class Account {
-	/**
-	 * 余额
-	 */
-	int money;
+    /**
+     * 余额
+     */
+    int money;
 
-	/**
-	 * 卡名
-	 */
-	String name;
+    /**
+     * 卡名
+     */
+    String name;
 
-	public Account(int money, String name) {
-		this.money = money;
-		this.name = name;
-	}
+    public Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
 }
 
 /**
  * 取钱
  */
 class WithdrawMoney extends Thread {
-	/**
-	 * 账户
-	 */
-	Account account;
-	/**
-	 * 取了多少钱
-	 */
-	int drawingMoney;
+    /**
+     * 账户
+     */
+    Account account;
+    /**
+     * 取了多少钱
+     */
+    int drawingMoney;
 
-	/**
-	 * 现在手里的钱
-	 */
-	int nowMoney;
+    /**
+     * 现在手里的钱
+     */
+    int nowMoney;
 
-	public WithdrawMoney(Account account, int drawingMoney, String name) {
-		super(name);
-		this.account = account;
-		this.drawingMoney = drawingMoney;
-	}
+    public WithdrawMoney(Account account, int drawingMoney, String name) {
+        super(name);
+        this.account = account;
+        this.drawingMoney = drawingMoney;
+    }
 
-	@Override
-	public void run() {
-		if (account.money - drawingMoney < 0) {
-			System.out.println("余额不足");
-		}
-		// sleep可以放大问题的发生性
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println(account.name + "余额为" + account.money);
-		nowMoney = nowMoney + drawingMoney;
-		System.out.println(this.getName() + "手里的钱--》" + nowMoney);
-		account.money = account.money - drawingMoney;
-		System.out.println(this.getName() + "取完钱，" + account.name + "-->还剩" + account.money);
-	}
+    @Override
+    public void run() {
+        // 锁的对象就是需要变化的量
+        synchronized (account) {
+            if (account.money - drawingMoney < 0) {
+                System.out.println("【" + this.getName() + "】想取" + drawingMoney + "，余额不足，剩余" + account.money);
+                return;
+            }
+            // sleep可以放大问题的发生性
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("【" + account.name + "】余额为" + account.money);
+            nowMoney = nowMoney + drawingMoney;
+            System.out.println("【" + this.getName() + "】取了--》" + nowMoney);
+            account.money = account.money - drawingMoney;
+            System.out.println("【" + this.getName() + "】取完钱，" + account.name + "-->还剩" + account.money);
+        }
+    }
 }
--- src/main/java/thread/UnSafeList.java
+++ src/main/java/thread/UnSafeList.java
@@ -13,7 +13,17 @@ public class UnSafeList {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < 10000; i++) {
-            new Thread(() -> list.add(Thread.currentThread().getName())).start();
+            new Thread(() -> {
+                synchronized (list) {
+                    list.add(Thread.currentThread().getName());
+                }
+            }).start();
+        }
+        // 如果这里不加延时，还是会数量不一致
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         // 打印结果都不是10000：多个线程同事操作一个对象，重复写入了
         System.out.println(list.size());
