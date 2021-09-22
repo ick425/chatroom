9ad28735c4c9e34346a56a8ff53b7440a913966b
王成龙 <15963685463@163.com>
2021-09-22 17:22:50 +0800

    线程不安全的集合

---
+++ src/main/java/thread/UnSafeList.java
@@ -0,0 +1,21 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 线程不安全的集合
+ * 多个线程同事操作一个对象，重复写入了
+ *
+ * @author wcl
+ */
+public class UnSafeList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            new Thread(() -> list.add(Thread.currentThread().getName())).start();
+        }
+        // 打印结果都不是10000：多个线程同事操作一个对象，重复写入了
+        System.out.println(list.size());
+    }
+}
