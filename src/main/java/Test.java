35a6feb6366d6143793db448ec663473547a8e8d
wangchenglong <wangcl821@163.com>
2022-05-24 10:35:46 +0800

    1480. 一维数组的动态和

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,16 +8,32 @@ public class Test {
 
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        String[] strings = new String[list.size()];
-        List<String> list1 = Arrays.asList(strings);
-        list1.set(0, "aaaa");
-        list1.set(2, "cccc");
+        System.out.println(Arrays.toString(runningSum()));
 
-        System.out.println(list1);
 
+    }
+
+    /**
+     * 1480. 一维数组的动态和
+     * 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+     * <p>
+     * 请返回 nums 的动态和。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/running-sum-of-1d-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * <p>
+     * 下标0：本次的值
+     * 下标>0：上次的累加值＋本次的值
+     * 下标从1开始，本次的累加值=上次累加+本次
+     *
+     * @return
+     */
+    public static int[] runningSum() {
+        int[] nums = new int[]{1, 2, 3, 4};
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
     }
 }
