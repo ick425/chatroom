806180596dd6da1f1bfff58a6e19b4aea56271a2
wangchenglong <wangcl821@163.com>
2024-02-23 11:38:40 +0800

    升级lombok版本

--- face-core/src/main/java/com/face/core/CoreApplication.java
+++ face-core/src/main/java/com/face/core/CoreApplication.java
@@ -1,19 +1,19 @@
-package com.face.core;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-/**
- * @author wo
- */
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class CoreApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
-        System.out.println("启动成功！");
-    }
-
-}
+//package com.face.core;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//
+///**
+// * @author wo
+// */
+//
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//public class CoreApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CoreApplication.class, args);
+//        System.out.println("启动成功！");
+//    }
+//
+//}
